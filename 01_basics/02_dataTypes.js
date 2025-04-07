1c89f338cba66ecc3ef4c4ceb87f5f29aad2f0be
aliahmedo6 <134384213+aliahmedo6@users.noreply.github.com>
2025-04-07 00:55:35 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // mot possible here, because we are using node js, not browser
+
+console.log(3+3); console.log("Test") //code readability should be high
+
+let name = "Test" //String Data Type
+let age = 18 //Number 
+let isLoggedIn = false // Boolean
+let state; //undefined
+let count = null //empty
+
+// primitive data types:
+// number => 2 to power 53
+// bigint => if big number e.g trading, facebook
+// String - ""
+// Boolean - true/false
+// null - standalone value
+// undefined - value is not defined/assigned
+// symbol - unique
+
+// object:
+console.log(typeof "test");
+console.log(typeof age);
+console.log(typeof null); //output: null is an object
+console.log(typeof undefined); //output: undefined
