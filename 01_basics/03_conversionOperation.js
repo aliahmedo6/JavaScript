f5146df45a502099ff9dc1484fe51145e19f7839
aliahmedo6 <134384213+aliahmedo6@users.noreply.github.com>
2025-04-08 02:22:04 +0000

    updated-conversion of dataTypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueInNumber = Number(score) //convert into number
-console.log(typeof valueInNumber)
+// console.log(typeof valueInNumber)
 
 // "33" - 33
 // "33abc" - Nan
@@ -12,7 +12,7 @@ console.log(typeof valueInNumber)
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean (isLoggedIn) //convert into boolean
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1-true; 0-false
 // "" - false
@@ -20,5 +20,25 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber) //convert into string
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+/* ---------------Operations --------------------- */
+let value = 3;
+let negVlaue = -value
+// console.log(negVlaue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) //2 raise to power 3
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = "test"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log((3+4)*5%3)
