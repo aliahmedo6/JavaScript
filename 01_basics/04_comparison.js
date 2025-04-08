71ed2ba59df2d6c4690a731f98fdc9f00a79f9e1
aliahmedo6 <134384213+aliahmedo6@users.noreply.github.com>
2025-04-08 02:50:21 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+//Basic Conversion
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2 < 1);
+// console.log(2 == 1)
+// console.log(2 != 1);
+
+// == - comparison
+// <,>,<=,>= - equality check
+//NOTE: Avoid these conversions
+console.log("2">1);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// === - Check Values and DataType
+console.log("2" === 2);
