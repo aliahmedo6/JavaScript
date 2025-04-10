4953ba57a6d498de875f8784e2aba1a1094cee33
aliahmedo6 <134384213+aliahmedo6@users.noreply.github.com>
2025-04-10 02:01:35 +0000

    math and num

---
+++ 01_basics/nums_and_math.js
@@ -0,0 +1,37 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number (100)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.9874
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//******************************************** */
+// console.log(Math);
+// console.log(Math.abs(-4)); //Absolute value - minus value updated to positive
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4,3,9,8));
+
+console.log(Math.random()); //output between 0-1
+console.log(Math.random()*10); //shift to left from decimal
+console.log((Math.random()*10) +1); //avoid 0 output - give value above 1-9
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() *(max - min + 1))+min);
+
+
+
+
+
+
